d80fbf2e906a247a9d4f600faf70334539bd67dd
v074 <buncherd01@gmail.com>
2021-02-22 12:09:15 -0600

    Uploading completed assignment

---
+++ jsBasicsExercise.js
@@ -0,0 +1,41 @@
+// 1: Have a console log display your favorite number in the console/terminal
+
+console.log(74);
+
+// 2: The five basic data types are Numbers, Strings, Booleans, Null, and Undefined.
+
+// 3: The six mathematical operators are + (Addition), - (Subtraction), * (Multiplication), / (Division), ** (Exponentiation), and % (Modulo)
+
+// 4: Use modulo to get a remainder of 1 from 5.  Post both ways.
+console.log(5%4);
+console.log(5%2);
+
+// 5: using single quotes, get "I'M MARY POPPINS, Y'ALL" to display in the terminal/console
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+// 6: Using Concatenation combine the following strings into one string. First string: “Wanda”. Second string: “Vision”.
+console.log("Wanda"+"Vision")
+
+// 7: Create a string for your first name and also your last name. Use the length property to add them together (Answer should be a number)
+console.log("Douglas".length+"Buncher".length);
+
+// 8: Use index on the following string to select the lowercase “e” at the end of the string. “Avengers: Endgame”. (Use a console log to display it in the console/terminal)
+console.log("Avengers: Endgame"[16])
+
+// B1: Create a string that displays only a backslash when it is displayed in the console/terminal
+console.log("\\");
+
+// B2: Use the following numbers in the order shown to get a result of the 616. Numbers are 5, 4, and 9 (HINT: Exponentiation)
+console.log(5**4-9);
+
+// B3: Use the following strings in order “I”, “Am”, “Iron”, and “Man” to display the number Zero in the console/terminal using the length property. (NOTE: There are multiple answers. For an extra bonus try to use modulo).
+console.log("I".length%"Am".length-"Iron".length%"Man".length);
+
+// B4: Using Index on each of the following strings, have AVENGERS display vertically in the console/terminal (Strings: “CAP” “VISION” “HAWKEYE” “NICK FURY” “GROOT” “DOCTOR STRANGE” “THOR” “STAR-LORD”).
+console.log("CAP"[1]+"VISION"[0]+"HAWKEYE"[6]+"NICK FURY"[0]+"GROOT"[0]+"DOCTOR STRANGE"[13]+"THOR"[3]+"STAR-LORD"[0]);
+
+// B5: Create the following variables with the following values m=10 and c=5. Create a variable with the name of u and set the value for it by adding x and y together (NOTE: Must add variables not 10 + 5 when setting value for the u variable).
+var m=10;
+var c=5;
+u=m+c;
+console.log(u);
